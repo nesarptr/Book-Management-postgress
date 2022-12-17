81739ff9f2764241cb2a99da9c426364d4cd99f2
Nesar Ahmad Patwary <nesarahmadpatwary@outlook.com>
2022-12-17 21:12:47 +0600

    application entry-point created

--- src/cmd/main/main.go
+++ src/cmd/main/main.go
@@ -1,3 +1,16 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/nesarptr/Book-Management-postgress/pkg/routes"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe(":3000", r))
+}
