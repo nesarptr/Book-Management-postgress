85af49ae3c03f110395ae4016ea9a28506b9a949
Nesar Ahmad Patwary <nesarahmadpatwary@outlook.com>
2022-12-17 21:06:31 +0600

    database connection function completed

--- src/pkg/config/app.go
+++ src/pkg/config/app.go
@@ -1,1 +1,38 @@
 package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
+func Connect() {
+
+	err := godotenv.Load()
+	if err != nil {
+		panic(err)
+	}
+
+	HOST := os.Getenv("HOST")
+	USER := os.Getenv("USER")
+	PASSWORD := os.Getenv("PASSWORD")
+	DATABASE := os.Getenv("DATABASE")
+	PORT := os.Getenv("PORT")
+	SSLMODE := os.Getenv("SSLMODE")
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s", HOST, USER, PASSWORD, DATABASE, PORT, SSLMODE)
+	d, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	db = d
+}
+
+func GetDB() *gorm.DB {
+	return db
+}
