ea671ab0e815713d25589fde0bc89337f6389ad7
Nesar Ahmad Patwary <nesarahmadpatwary@outlook.com>
2022-12-17 20:46:25 +0600

    utility function for parsing request body added

--- src/pkg/utils/utils.go
+++ src/pkg/utils/utils.go
@@ -1,1 +1,17 @@
 package utils
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+func ParseBody(r *http.Request, x interface{}) {
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		panic(err)
+	}
+	if err := json.Unmarshal(body, x); err != nil {
+		panic(err)
+	}
+}
