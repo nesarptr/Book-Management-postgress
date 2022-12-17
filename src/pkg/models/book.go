c6e53ccb24f14336c0841d68a734981264742595
Nesar Ahmad Patwary <nesarahmadpatwary@outlook.com>
2022-12-17 20:51:47 +0600

    models functionality added

--- src/pkg/models/book.go
+++ src/pkg/models/book.go
@@ -1,1 +1,45 @@
 package models
+
+import (
+	"github.com/nesarptr/Book-Management-postgress/pkg/config"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
+type Book struct {
+	gorm.Model
+	Name        string `json:"name"`
+	Author      string `json:"author"`
+	Publication string `json:"publication"`
+}
+
+func (book *Book) CreateBook() *Book {
+	db.Create(book)
+	return book
+}
+
+func GetAllBooks() *[]Book {
+	var Books []Book
+	db.Find(&Books)
+	return &Books
+}
+
+func GetBookById(id uint) (*Book, *gorm.DB) {
+	var Book Book
+	db := db.First(&Book, id)
+	return &Book, db
+}
+
+func DeleteById(id uint) *Book {
+	var Book Book
+	db.Delete(&Book, id)
+	Book.ID = id
+	return &Book
+}
+
+func init() {
+	config.Connect()
+	db = config.GetDB()
+	db.AutoMigrate(&Book{})
+}
