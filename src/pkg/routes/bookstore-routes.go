1a98c19ee563cef331c01c844d699aa35ecccc8c
Nesar Ahmad Patwary <nesarahmadpatwary@outlook.com>
2022-12-17 20:44:25 +0600

    Routes added

--- src/pkg/routes/bookstore-routes.go
+++ src/pkg/routes/bookstore-routes.go
@@ -1,1 +1,14 @@
 package routes
+
+import (
+	"github.com/gorilla/mux"
+	"github.com/nesarptr/Book-Management-postgress/pkg/controllers"
+)
+
+func RegisterBookStoreRoutes(router *mux.Router) {
+	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
+	router.HandleFunc("/book/", controllers.GetBooks).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.GetBookById).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.UpdateBook).Methods("PUT")
+	router.HandleFunc("/book/{bookId}", controllers.DeleteBook).Methods("DELETE")
+}
